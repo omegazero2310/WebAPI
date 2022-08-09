48e6ee00adf40c7f5ef827b87b4630df7eb62aa0
annv3 <annv3@TS0813423-PM-I5>
2022-08-09 10:19:47 +0700

    Add project files.

---
+++ WebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Linq;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+
+        /// <summary>The list employee</summary>
+        /// <Modified>
+        /// Name Date Comments
+        /// annv3 8/9/2022 created
+        /// </Modified>
+        private static List<POCOClass.Employee> _ListEmployee = new List<POCOClass.Employee>() { new POCOClass.Employee() { Id = "1", Name = "An", DateOfBirth = DateTime.Now, ExtraInfo = "123" } };
+
+        // GET: api/<EmployeeController>
+        [HttpGet]
+        public IEnumerable<POCOClass.Employee> Get()
+        {
+            return _ListEmployee;
+        }
+
+        // GET api/<EmployeeController>/5
+        [HttpGet("{id}")]
+        public POCOClass.Employee Get(string id)
+        {
+            return _ListEmployee.Where(emp => emp.Id == id).FirstOrDefault();
+        }
+
+        // POST api/<EmployeeController>
+        [HttpPost]
+        public void Post([FromBody] POCOClass.Employee value)
+        {
+            if (!_ListEmployee.Any(emp => emp.Id == value.Id))
+                _ListEmployee.Add(value);
+        }
+
+        // PUT api/<EmployeeController>/5
+        [HttpPut("{id}")]
+        public void Put(string id, [FromBody] POCOClass.Employee value)
+        {
+            int index = _ListEmployee.FindIndex(emp => emp.Id == value.Id);
+            if (index != -1)
+                _ListEmployee[index] = value;
+        }
+
+        // DELETE api/<EmployeeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            int index = _ListEmployee.FindIndex(emp => emp.Id == id);
+            if (index != -1)
+                _ListEmployee.RemoveAt(index);
+        }
+    }
+}
---
+++ WebAPI/POCOClass/Employee.cs
@@ -0,0 +1,20 @@
+﻿namespace WebAPI.POCOClass
+{
+
+    /// <summary>
+    ///     Lưu trữ thông tin nhân viên
+    ///   <br />
+    /// </summary>
+    /// <Modified>
+    /// Name Date Comments
+    /// annv3 8/9/2022 created
+    /// </Modified>
+    public class Employee
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string ExtraInfo { get; set; }
+
+    }
+}
